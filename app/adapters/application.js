e66ee0c653e1b31f1b1450db688653206df4f951
Lokesh Jangid <lokesh@opinioapp.com>
2014-11-11 22:54:34 +0530

    working with pouchdb

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,3 +1,9 @@
-import DS from 'ember-data';
+//import DS from 'ember-data';
 
-export default DS.FixtureAdapter.extend();
+//export default DS.FixtureAdapter.extend();
+var db = new PouchDB('mydb');
+//db.sync('http://localhost:5984/mydb', {live: true});
+
+export default EmberPouch.Adapter.extend({
+  db: db
+});
--- app/models/todo.js
+++ app/models/todo.js
@@ -3,24 +3,25 @@ import DS from 'ember-data';
 
 var Todo = DS.Model.extend({
   title: DS.attr('string'),
-  isCompleted: DS.attr('boolean')
+  isCompleted: DS.attr('boolean'),
+  rev: DS.attr('string')
 });
 
-Todo.reopenClass({
-  FIXTURES: [
-  {
-    id: "1",
-    title: 'install ember-cli',
-    isCompleted: true
-  }, {
-    id: "2",
-    title: 'install additional dependencies',
-    isCompleted: true
-  }, {
-    id: "3",
-    title: 'develop amazing things',
-    isCompleted: false
-  }
-]});
+//Todo.reopenClass({
+//  FIXTURES: [
+//    {
+//      id: "1",
+//      title: 'install ember-cli',
+//      isCompleted: true
+//    }, {
+//      id: "2",
+//      title: 'install additional dependencies',
+//      isCompleted: true
+//    }, {
+//      id: "3",
+//      title: 'develop amazing things',
+//      isCompleted: false
+//    }
+//  ]});
 
 export default Todo;
